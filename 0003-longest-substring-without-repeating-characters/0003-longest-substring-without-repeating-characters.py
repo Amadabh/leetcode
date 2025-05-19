fa504b0196087af639ed446710a10602eede8ce4
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-05-19 09:31:58 -0700

    Time: 292 ms (6.85%), Space: 17.8 MB (52.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,50 +1,37 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        l=0
-        if s=="":
-            return 0
-        st = set()
-        st.add(s[l])
+        #Brute force
         ans =0
-        for r in range(1,n):
-            if s[r] not in st:
-                st.add(s[r])
-            else:
-                ans = max(len(st),ans)
-                while s[r] in st:
-                    st.remove(s[l])
-                    l+=1
-                st.add(s[r])
-        ans = max(len(st),ans)
+        n = len(s)
+        for i in range(n):
+            st = set()
+            for j in range(i,n):
+                if s[j] in st:
+                    break
+                st.add(s[j])
+            ans = max(len(st),ans)
         return ans
 
 
-
-
-        # def rec(i,path):
-           
-        #     if i == len(s) or s[i] in path:
-        #         return len(path)
-
-        #     path.add(s[i])
-        #     result = rec(i + 1, path)
-        #     path.remove(s[i])
-        #     return result
-
-        # return rec(0,set())
-
-            
-
-        # m = 0
-        # st= set()
-        # for i in s:
-        #     if i not in st:
-        #         st.add(i)
+        #OPtimal
+        # n = len(s)
+        # l=0
+        # if s=="":
+        #     return 0
+        # st = set()
+        # st.add(s[l])
+        # ans =0
+        # for r in range(1,n):
+        #     if s[r] not in st:
+        #         st.add(s[r])
         #     else:
-        #         m = max(len(st),m)
-        #         st.remove(i)
-        #         st.add(i)
-        # m = max(len(st),m)
-        # return m
-        
+        #         ans = max(len(st),ans)
+        #         while s[r] in st:
+        #             st.remove(s[l])
+        #             l+=1
+        #         st.add(s[r])
+        # ans = max(len(st),ans)
+        # return ans
+
+
+
