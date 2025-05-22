d16e38fda5ebbc2776c945104ef731235bce103b
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-05-22 11:44:05 -0700

    Time: 7 ms (89.77%), Space: 18 MB (48.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        d1=defaultdict(int)
+        for i in s:
+            d1[i]+=1
+        d2 = defaultdict(int)
+        for i in t:
+            d2[i]+=1
+        return d1==d2
+
+        #Sorted solution
+        # return sorted(s) == sorted(t)
         
