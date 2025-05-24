d51a27e09d69fa6fd676dd5c93b3109d5910b6c1
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-05-24 16:26:49 -0400

    Time: 52 ms (53.18%), Space: 34.2 MB (53.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,25 +1,34 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0 
-        nums = list(set(nums))
-        heap = nums.copy()
-        heapq.heapify(heap)
+        numset = set(nums)
+        maxlen = 0
+        for n in numset:
+            if (n-1) not in numset:
+                length=0
+                while (n+length) in numset:
+                    length+=1
+                maxlen= max(length,maxlen)
+        return maxlen
+        # if not nums:
+        #     return 0 
+        # nums = list(set(nums))
+        # heap = nums.copy()
+        # heapq.heapify(heap)
 
-        maxSeq = 1
-        prev = nums[0]
-        cnt=1
-        while heap:
-            ele = heapq.heappop(heap)
-            # print(ele,cnt)?
-            if ele==prev+1:
-                cnt+=1
-            else:
-                maxSeq = max(cnt,maxSeq)
-                cnt = 1
-            prev = ele
-        maxSeq = max(cnt,maxSeq)
+        # maxSeq = 1
+        # prev = nums[0]
+        # cnt=1
+        # while heap:
+        #     ele = heapq.heappop(heap)
+        #     # print(ele,cnt)?
+        #     if ele==prev+1:
+        #         cnt+=1
+        #     else:
+        #         maxSeq = max(cnt,maxSeq)
+        #         cnt = 1
+        #     prev = ele
+        # maxSeq = max(cnt,maxSeq)
 
-        return maxSeq
+        # return maxSeq
 
         
