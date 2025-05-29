7006cb427e5d55a9639e33bfb03f6fb4361c85b7
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-05-29 14:32:05 -0400

    Time: 1 ms (43.33%), Space: 18.1 MB (43.82%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans =[]
+        str_maxlen = 2*n
+        def rec(num, open, st):
+            if len(st)==str_maxlen:
+                ans.append(st)
+                return
+
+            if num==0:
+            #wjhile loop close: based on len of the string in between
+                while len(st)<str_maxlen:
+                    st+=")"
+                ans.append(st)
+                return
+
+            if num>0:
+                #open
+                rec(num-1,open+1,st+"(")
+            if open>0:
+                #close
+                rec(num,open-1,st+")")
+                
+        rec(n,0,"")
+        return ans
+        
