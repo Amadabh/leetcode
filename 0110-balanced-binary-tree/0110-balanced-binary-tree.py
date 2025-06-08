fb344d474bb7870b1f0139d2cfc915f6c525ae5f
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-06-08 15:16:49 -0400

    Time: 5 ms (38.4%), Space: 19 MB (7.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,28 +6,47 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def height(root):
-            if not root:
-                return 0
-            l = height(root.left)
-            r = height(root.right)
-            return 1 + max(l,r)
-        
         def balance(root):
             if not root:
-                return True
+                return 0
             left = balance(root.left)
             right = balance(root.right)
+
+            if left==-1 or right==-1:
+                return -1
+
+            res = abs(left - right)
+            if res<2:
+                return 1+max(left,right)
+            else:
+                return -1
+        return balance(root)!=-1
+           
+
             
-            l = height(root.left)
-            r = height(root.right)
+        #Brute force check height of each node
+        # def height(root):
+        #     if not root:
+        #         return 0
+        #     l = height(root.left)
+        #     r = height(root.right)
+        #     return 1 + max(l,r)
+        
+        # def balance(root):
+        #     if not root:
+        #         return True
+        #     left = balance(root.left)
+        #     right = balance(root.right)
+            
+        #     l = height(root.left)
+        #     r = height(root.right)
           
-            res = abs(l-r)
-            bl = False
-            if res<=1:
-                bl = True
-            return bl and left and right
-        return balance(root)
+        #     res = abs(l-r)
+        #     bl = False
+        #     if res<=1:
+        #         bl = True
+        #     return bl and left and right
+        # return balance(root)
             
 
 
