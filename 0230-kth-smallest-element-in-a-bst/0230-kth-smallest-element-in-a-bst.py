3eef17bd5584d0da1308dd6887e22a7e72d4f132
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-06-17 17:23:48 -0400

    Time: 2 ms (41.33%), Space: 21.1 MB (27.19%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -8,13 +8,27 @@ class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         #Brute force traverse the entire tree store it in an array sort it get kth index or use heap to get kth val
         arr = []
+        heapq.heapify(arr)
         def dfs(root):
             if not root:
                 return
-            arr.append(root.val)
+            heapq.heappush(arr,root.val)
             dfs(root.left)
             dfs(root.right)
         dfs(root)
-        arr.sort()
-        return arr[k-1]
+        ans =0
+        for i in range(k):
+            ans = heapq.heappop(arr)
+        return ans
+        # Array
+        # arr = []
+        # def dfs(root):
+        #     if not root:
+        #         return
+        #     arr.append(root.val)
+        #     dfs(root.left)
+        #     dfs(root.right)
+        # dfs(root)
+        # arr.sort()
+        # return arr[k-1]
         
