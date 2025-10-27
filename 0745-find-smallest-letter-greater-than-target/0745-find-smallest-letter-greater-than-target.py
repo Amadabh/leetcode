3e8f08ca400edacb63aa03cbe932d29f922381cb
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-10-26 18:16:44 -0700

    Time: 0 ms (100%), Space: 13.6 MB (94.74%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.py
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.py
@@ -8,22 +8,32 @@ class Solution(object):
         n = len(letters)
         nums = letters
         l, r = 0, n - 1
+        pos = -1
         while l<=r:
-            mid = (l+r)//2
-            if nums[mid]==target:
-
-                while mid+1 < n and nums[mid+1] ==target:
-                    mid+=1
-                if mid == n-1:
-                    return nums[0]
-                print(mid)
-                return nums[mid+1]
-            elif nums[mid]<target:
-                l = mid + 1
+            mid =  (l+r)//2
+            if nums[mid] > target:
+                pos = mid
+                r = mid -1
             else:
-                r = mid - 1
-        print(l)
-        if l>n-1:
-            return letters[0]
-        return letters[l]
+                l = mid+1
+        return letters[0] if pos==-1 else letters[pos]
+
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     if nums[mid]==target:
+
+        #         while mid+1 < n and nums[mid+1] ==target:
+        #             mid+=1
+        #         if mid == n-1:
+        #             return nums[0]
+        #         print(mid)
+        #         return nums[mid+1]
+        #     elif nums[mid]<target:
+        #         l = mid + 1
+        #     else:
+        #         r = mid - 1
+        # print(l)
+        # if l>n-1:
+        #     return letters[0]
+        # return letters[l]
         
