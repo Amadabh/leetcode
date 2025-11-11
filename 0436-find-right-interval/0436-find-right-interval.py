d7b79e6e3ec82c49b43c77580538f0f64b98ca08
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-11-10 21:24:09 -0800

    Time: 103 ms (11.27%), Space: 17.9 MB (5.63%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.py
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -14,16 +14,29 @@ class Solution(object):
         arr = sorted(intervals, key = lambda x: x[0])
         # print(arr)
         for i in range(n):
-            m = float("inf")
-            ind = -1
-            for j in range(i,n):
-                # print(intervals[j])
-                if arr[i][1] <= arr[j][0]:
-                    # print(intervals[i][1], intervals[j][0])
-                    ind = pos[tuple(arr[j])]
-                    break
+            start, end = arr[i]
+            # for j in range(i,n):
+            #     # print(intervals[j])
+            #     if arr[i][1] <= arr[j][0]:
+            #         # print(intervals[i][1], intervals[j][0])
+            #         ind = pos[tuple(arr[j])]
+            #         break
 
-            ans[pos[tuple( arr[i] ) ]] = ind
+            l, r = 0, n - 1
+            found = -1
+
+            while l <= r:
+                mid = (l + r) // 2
+                if arr[mid][0] >= end:
+                    found = mid
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
+            if found != -1:
+                found = pos[tuple(arr[found])]
+
+            ans[pos[tuple( arr[i] ) ]] = found
         return ans
 
         # ans = []
