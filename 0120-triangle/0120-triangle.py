8b3244a9bf055c62ca5ee556fa6d037f538979cd
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-12-06 16:49:48 -0800

    Time: 0 ms (100%), Space: 18.6 MB (98.02%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,20 +1,32 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # m = len(triangle)
+        # arr = triangle
+        # ans = [0]
+        # dp = [[None for i in range(len(arr[j]))] for j in range(m)]
+        # def rec(i, j) -> int:
+        #     if i == m-1:
+        #         return arr[i][j]
+        #     if dp[i][j] is not None:
+        #         return dp[i][j]
+        #     p = rec(i + 1, j)
+        #     p_next = rec(i + 1, j + 1)
+        #     ans = arr[i][j] + min(p, p_next)
+        #     dp[i][j] = ans
+        #     return ans
+        # return rec(0, 0)
+
+        # O(n)
+
         m = len(triangle)
-        arr = triangle
-        ans = [0]
-        dp = [[None for i in range(len(arr[j]))] for j in range(m)]
-        def rec(i, j) -> int:
-            if i == m-1:
-                return arr[i][j]
-            if dp[i][j] is not None:
-                return dp[i][j]
-            p = rec(i + 1, j)
-            p_next = rec(i + 1, j + 1)
-            ans = arr[i][j] + min(p, p_next)
-            dp[i][j] = ans
-            return ans
-        return rec(0, 0)
+        dp = triangle[-1][:].copy()
+        for i in range(m-2, -1, -1):
+            for j in range(i + 1):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1])
+        return dp[0]
+    
+
+
 
 
 
