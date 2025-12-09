fd5008e61303cb514030212fb2d8f6e307389274
Amadabh <84470487+Amadabh@users.noreply.github.com>
2025-12-09 12:15:18 -0800

    Time: 43 ms (5.02%), Space: 28.3 MB (5%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,49 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #Recursion
+        # if prices == sorted(prices, reverse=True):
+        #     return 0
+        # n = len(prices)
+        # dp = [[float("inf") for j in range(2)] for i in range(n)]
+        
+        # def rec(buy, ind):
+        #     if ind == n:
+        #         return 0
+        #     take = 0
+        #     notTake = rec(buy, ind + 1)
+        #     if dp[ind][buy]!= float("inf"):
+        #         return dp[ind][buy]
+
+        #     if buy:
+        #         take = rec(False, ind + 1) - prices[ind]
+        #     else:
+        #         take = rec(True, ind + 1) + prices[ind]
+            
+        #     dp[ind][buy] = max(take, notTake)
+        #     return dp[ind][buy]
+        # return rec(True, 0)
+
+        # Tabulation
+
+        if prices == sorted(prices, reverse=True):
+            return 0
+        n = len(prices)
+        dp = [[float("inf") for j in range(2)] for i in range(n)]
+        
+        def rec(buy, ind):
+            if ind == n:
+                return 0
+            take = 0
+            if dp[ind][buy]!= float("inf"):
+                return dp[ind][buy]
+            notTake = rec(buy, ind + 1)
+           
+
+            if buy:
+                take = rec(False, ind + 1) - prices[ind]
+            else:
+                take = rec(True, ind + 1) + prices[ind]
+            
+            dp[ind][buy] = max(take, notTake)
+            return dp[ind][buy]
+        return rec(True, 0)
