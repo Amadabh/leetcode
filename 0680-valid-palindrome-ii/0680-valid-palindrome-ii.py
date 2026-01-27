d0c0f77a8ed6711a17546c70993c5f87bc500447
Amadabh <84470487+Amadabh@users.noreply.github.com>
2026-01-27 15:09:36 -0800

    Time: 23 ms (97.21%), Space: 19.7 MB (21.84%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -2,16 +2,26 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         
         n = len(s)
-        rev = s[::-1]
-        if s== rev:
-            return True
-        def isPalin(st : str, rst: str) -> str:
-            return st == rst
-        
-        for i in range(n):
-            if isPalin(s[:i] + s[i+1:], rev[:n - i - 1] + rev[n-i:]):
-                return True
-            # print(s[:i] + s[i+1:], rev[:n - i - 1] + rev[n-i:])
-        return False
+    
+        def isPalin(st : str) -> str:
+            return st == st[::-1]
+        i, j = 0, n - 1
+
+        while i < j:
+            if s[i] != s[j]:
+                return isPalin(s[:i] + s[i+1:]) or isPalin(s[:j] + s[j+1:])
+            i+=1
+            j-=1
+
+        return True
+
+
+            # def isPalin(st : str, rst: str) -> str:
+            # return st == rst
+        # for i in range(n):
+        #     if isPalin(s[:i] + s[i+1:], rev[:n - i - 1] + rev[n-i:]):
+        #         return True
+        #     # print(s[:i] + s[i+1:], rev[:n - i - 1] + rev[n-i:])
+        # return False
 
     
